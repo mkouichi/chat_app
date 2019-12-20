1e2b337091b43e8982f52e94ff14f5aa7b4727f5
mkouichi <k-m2011@hotmail.co.jp>
2019-12-21 00:46:53 +0200

    Modify DB config

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express"),
     socket = require("socket.io");
 
 // DB Config
-const db = require(`mongodb+srv://${process.env.username}:${process.env.password}.@chat-app-node-socket-0gxnd.mongodb.net/test?retryWrites=true&w=majority`)
-        .mongoURI,
+const db = require(`mongodb+srv://${process.env.username}:${process.env.password}.@chat-app-node-socket-0gxnd.mongodb.net/test?retryWrites=true&w=majority`),
     mongoose = require("mongoose");
 
 // App setup
